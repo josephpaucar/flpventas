8fb55a5fbc178fdeba38e07fd454ca7c0c5053f4
josephpaucar <mailme@josephpaucar.dev>
2023-03-29 22:56:01 -0500

    Se agrego tailwindcss y se limpio el proyecto

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div class="about">
+      <h1 class="text-3xl font-bold underline">This is the home page</h1>
+    </div>
   </main>
 </template>
