09698184da868b402783fa0aaabdfcb3a75336db
Joseph Paucar <mailme@josephpaucar.dev>
2023-04-01 22:16:36 -0500

    Compoente headertop creado

---
+++ src/components/TopbarHeader.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="container flex justify-between">
+    <div class="flex">
+        <p>Envío GRATIS por compras mayores a S/.150</p>
+        <p>Llámanos al 982 466 390</p>
+    </div>
+    <div class="flex">
+        <a href="#">Mi Cuenta</a>
+        <a href="#">Wishlist</a>
+        <div class="separator"></div>
+        <a href="#">facebook</a>
+        <a href="#">Instagram</a>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
